2488c26fbfe44296dc193d0ca0da6379b5996aa7
Stire <stire.ru@gmail.com>
2020-03-30 01:37:07 +0300

    App update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
+import { Provider } from 'overmind-react'
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
+import { List } from './components/List/List'
+import { overmind } from './overmind'
+
+const App = () => (
+  <Provider value={overmind}>
+    <List />
+  </Provider>
+)
 
 export default App
--- src/overmind/actions.ts
+++ src/overmind/actions.ts
@@ -14,11 +14,16 @@ export const setActive: Action<string> = ({ state }, id) => {
     let check = true
     const newActive: string[] = []
 
+    // iterate over all currently active items
     state.active.forEach((activeId) => {
+      // Set `active` field of iterated item as `true` if code didnt reach clicked item
+      // and as `false` if reached
       state.items[activeId].active = check
 
+      // If item is active, then push its id to new active array
       check && newActive.push(activeId)
 
+      // if `true`, then set `active` field for all next items as `false`
       if (activeId === id) {
         check = false
       }
@@ -28,36 +33,83 @@ export const setActive: Action<string> = ({ state }, id) => {
     return
   }
 
-  //2. Id is direct acsendant of last selected or first item
-  const startId = state.active[state.active.length - 1]
-  let selected = state.items[startId]
-  let forkCheck = selected.children.length === 1
-  const newActive: string[] = [...state.active]
+  // 3. Id is direct ascendand, climb from clicked item
 
-  while (forkCheck) {
-    selected = state.items[selected.children[0]]
-    newActive.push(selected.id)
-    forkCheck = selected.children.length === 1
+  // 2. Id is direct acsendant of last selected or first item
+  {
+    // Select last active item (by default root is active)
+    const startId = state.active[state.active.length - 1]
+    let selected = state.items[startId]
+    // Set check for fork - if there are multiple direct descendands then we are at fork
+    let forkCheck = selected.children.length === 1
+    // Creating new active array
+    const newActive: string[] = [...state.active]
 
+    // If we are not at fork then
+    while (forkCheck) {
+      // Select descendand
+      selected = state.items[selected.children[0]]
+      // Push his id to new active array
+      newActive.push(selected.id)
+      // Check for his descendands (fork)
+      forkCheck = selected.children.length === 1
+
+      // However if we are at clicked item, then leave cycle
+      if (selected.id === id) {
+        // break;
+        forkCheck = false
+      }
+    }
+
+    // If we are at fork, but clicked item is direct descendand
+    if (selected.children.includes(id)) {
+      // Add him to array and set flag as true for this case
+      newActive.push(id)
+      forkCheck = true
+    }
+
+    // If we are at clicked item
     if (selected.id === id) {
-      forkCheck = false
+      // Set flag as true for this case
+      forkCheck = true
+    }
+
+    // If this case is right, then update state and add active field as `true`
+    // for newly added items
+    if (forkCheck) {
+      state.active = newActive
+      newActive.forEach((active) => (state.items[active].active = true))
+      return
     }
   }
 
-  if (selected.children.includes(id)) {
-    newActive.push(id)
-    forkCheck = true
-  }
+  // 4. Id is in sibling branch of currently active path
+  // {
+  //   let selected = state.items[id]
+  //   const items = Object.values(state.items)
+  //   let parents = items.filter((item) => item.children.includes(selected.id))
+  //   let forkCheck = parents.length === 1
 
-  if (selected.id === id) {
-    forkCheck = true
-  }
+  //   if (parents.length === 1 && state.active.includes(parents[0].id)) {
+  //     const parent = parents[0]
+  //     forkCheck = false
+  //     const indexOfParent = state.active.indexOf(parent.id)
+  //     state.active = [...state.active.slice(0, indexOfParent), id]
+  //   }
 
-  console.log(newActive)
+  //   while (forkCheck) {
+  //     parents = items.filter((item) => item.children.includes(selected.id))
+  //     forkCheck = parent.length === 1
+  //   }
 
-  if (forkCheck) {
-    state.active = newActive
-    newActive.forEach((active) => (state.items[active].active = true))
-    return
-  }
+  //   const newActive = [...state.active]
+
+  //   if (forkCheck) {
+  //     state.active = newActive
+  //     newActive.forEach((active) => (state.items[active].active = true))
+  //     return
+  //   }
+  // }
+
+  return
 }
