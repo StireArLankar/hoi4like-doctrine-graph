f20ace17c68e7008a0a24292a699a46b2350324e
Stire <stire.ru@gmail.com>
2020-03-31 00:31:50 +0300

    Comments update

--- src/overmind/actions.ts
+++ src/overmind/actions.ts
@@ -9,6 +9,10 @@ export const decreaseCount: Action = ({ state }) => {
 }
 
 export const setActive: Action<string> = ({ state }, id) => {
+  if (state.active[state.active.length - 1] === id) {
+    return
+  }
+
   // 1. Id is inside active
   if (state.active.includes(id)) {
     let check = true
@@ -36,13 +40,19 @@ export const setActive: Action<string> = ({ state }, id) => {
 
   // 2. Id is direct ascendand, climb from clicked item
   {
+    // Select clicked item
     let selected = state.items[id]
+    // Convert items to array
     const items = Object.values(state.items)
+    // Find parents of clicked item
     let parents = items.filter((item) => item.children.includes(selected.id))
+    // Add clicked item to new active array
     let newActive: string[] = [id]
 
+    // Check if any parent is in active array
     let complete = parents.some((parent) => {
       const parentIsActive = state.active.includes(parent.id)
+      // If parent is in active array, then add ++++++ and set complete to true
       if (parentIsActive) {
         newActive.unshift(parent.id)
         return true
@@ -51,13 +61,19 @@ export const setActive: Action<string> = ({ state }, id) => {
       return false
     })
 
+    // Check for multiple parents
     let forkCheck = parents.length === 1
 
+    // While selected has one parent and none of his parents is in active array
     while (forkCheck && !complete) {
+      // Select parent (he is only one)
       selected = state.items[parents[0].id]
+      // Unshift his id to array
       newActive.unshift(selected.id)
+      // Find his parents
       parents = items.filter((item) => item.children.includes(selected.id))
 
+      // Check if any parent is in active array
       complete = parents.some((parent) => {
         const parentIsActive = state.active.includes(parent.id)
         if (parentIsActive) {
@@ -68,10 +84,13 @@ export const setActive: Action<string> = ({ state }, id) => {
         return false
       })
 
+      // Check for multiple parents
       forkCheck = parents.length === 1
     }
 
+    // If found parent that is in active array
     if (complete) {
+      // Slice active array from start to parent and set others active to `false`
       const indexOfParent = state.active.indexOf(newActive[0])
       const oldActive = [...state.active.slice(0, indexOfParent)]
 
@@ -79,46 +98,56 @@ export const setActive: Action<string> = ({ state }, id) => {
         state.items[state.active[i]].active = false
       }
 
+      // For new array set active to true
       newActive.forEach((id) => {
         state.items[id].active !== true && (state.items[id].active = true)
       })
 
+      // Update active array
       state.active = [...oldActive, ...newActive]
       return
     }
 
     // 3. Try to descend from last active item to fork
+    // Last item in active array
     const startId = state.active[state.active.length - 1]
     let selectedDown = state.items[startId]
+    // Check for multiple children
     let forkCheckDown = selectedDown.children.length === 1
+    // Creating increased array
     const newActiveDown: string[] = [...state.active]
 
-    let meeting = selectedDown.id === selected.id
+    // Set check to false as we didnt reach startId from clicked item
+    let meeting = false
 
+    // While no meeting and only one child
     while (forkCheckDown && !meeting) {
-      // Select descendand
+      // Select child
       selectedDown = state.items[selectedDown.children[0]]
       // Push his id to new active array
       newActiveDown.push(selectedDown.id)
-      // Check for his descendands (fork)
+      // Check for his children amount (fork)
       forkCheckDown = selectedDown.children.length === 1
 
-      // However if we are at clicked item, then leave cycle
+      // If newly selected item has selected id from prev case as his child
       if (selectedDown.children.includes(selected.id)) {
-        // break;
+        // MEETING!
         meeting = true
       }
     }
 
     if (meeting) {
+      // Set top chain items as active
       const indexOfParent = state.active.indexOf(startId)
 
       for (let i = indexOfParent + 1; i < newActiveDown.length; i++) {
         state.items[newActiveDown[i]].active = true
       }
 
+      // Set bottom chain items as active
       newActive.forEach((id) => (state.items[id].active = true))
 
+      // Update active array
       state.active = [...newActiveDown, ...newActive]
       return
     }
